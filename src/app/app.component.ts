35f7026c9751931f8d16fc94a47c82304c671c7d
Garvit Bhatia <garvitbhatia12@gmailcom>
2024-03-24 21:35:55 +0530

    final comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,18 @@
-import { Component } from '@angular/core';
-
+import {Component, Inject} from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import { NgOptimizedImage } from '@angular/common'
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef, MatDialogTitle
+} from "@angular/material/dialog";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {FormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +20,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Darbari';
+  public name: string = '';
+  public enter: boolean = false;
+  constructor(private _snackBar: MatSnackBar, public dialog: MatDialog) {
+  }
+
+  onClick() {
+    if (this.name.toUpperCase() === 'MANASVI' || this.name.toUpperCase() === 'DARBARI' || this.name.toUpperCase() === 'MANASVI DARBARI') {
+      this.enter = true;
+    } else {
+      this.openSnackBar();
+    }
+  }
+
+  openSnackBar() {
+    this._snackBar.open('Wrong Person! You are not allowed here', 'Close');
+  }
+
+  onExit() {
+    this._snackBar.open(`${this.name}, You are not allowed to Exit! you are stuck here forever`, 'Okay');
+  }
+
+  onMsg() {
+    this.dialog.open(DialogOverviewExampleDialog, {
+      height: '310px',
+      width: '375px'
+    });
+  }
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: 'dialog-overview-example-dialog.html',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+  ],
+})
+export class DialogOverviewExampleDialog {
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+  @Inject(MAT_DIALOG_DATA) public data: any,
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatSlideToggle} from "@angular/material/slide-toggle";
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
+import {MatButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import {MatGridList, MatGridTile} from "@angular/material/grid-list";
+import {NgOptimizedImage} from "@angular/common";
+import {MatBadge} from "@angular/material/badge";
 
 @NgModule({
   declarations: [
@@ -10,9 +20,22 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormField,
+    MatLabel,
+    MatSlideToggle,
+    MatInputModule,
+    FormsModule,
+    MatButton,
+    MatIcon,
+    MatGridList,
+    MatGridTile,
+    NgOptimizedImage,
+    MatBadge
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
